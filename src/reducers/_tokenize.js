21f8a748075ba690d9167be323901ed8bfee59f6
Pedro Gomes <pedrogomes94@gmail.com>
2018-08-12 11:59:37 +0100

    cleanup

--- src/reducers/_tokenize.js
+++ src/reducers/_tokenize.js
@@ -1,24 +1,68 @@
 import Web3 from 'web3';
+import { formatENSDomain } from '../helpers/utilities';
 import { transferName } from '../helpers/contracts/registrar';
 import { mintToken, getNextRegisterStep } from '../helpers/contracts/nifty';
 // -- Constants ------------------------------------------------------------- //
 const TOKENIZE_UPDATE_INPUT = 'tokenize/TOKENIZE_UPDATE_INPUT';
+
 const MINT_TOKEN_STATUS = 'tokenize/MINT_TOKEN_STATUS';
+
 const TRANSFER_NAME_STATUS = 'tokenize/TRANSFER_NAME_STATUS';
+
+const BURN_TOKEN_STATUS = 'tokenize/BURN_TOKEN_STATUS';
 
 // -- Actions --------------------------------------------------------------- //
 export const tokenizeUpdateInput = (input = '') => dispatch => {
   dispatch({ type: TOKENIZE_UPDATE_INPUT, payload: input });
 };
 
-function formatDomain(name) {
-  if (name.endsWith('.eth')) {
-    return name;
-  } else return name + '.eth';
-}
-
 export const tokenizeSubmitTransaction = name => async dispatch => {
-  const label = formatDomain(name).match(/(.*)\.eth/)[1];
+  const label = formatENSDomain(name).match(/(.*)\.eth/)[1];
+  const step = await getNextRegisterStep(Web3.utils.keccak256(label));
+  switch (step) {
+    case 'transfer':
+      dispatch({ type: TRANSFER_NAME_STATUS, payload: 'pending' });
+      transferName(Web3.utils.keccak256(label), () => {
+        dispatch({
+          type: TRANSFER_NAME_STATUS,
+          payload: 'success'
+        });
+        dispatch({ type: MINT_TOKEN_STATUS, payload: 'pending' });
+        mintToken(Web3.utils.keccak256(label), () =>
+          dispatch({
+            type: MINT_TOKEN_STATUS,
+            payload: 'success'
+          })
+        );
+      });
+      break;
+    case 'mint':
+      dispatch({
+        type: TRANSFER_NAME_STATUS,
+        payload: 'success'
+      });
+      dispatch({ type: MINT_TOKEN_STATUS, payload: 'pending' });
+      mintToken(Web3.utils.keccak256(label), () =>
+        dispatch({
+          type: MINT_TOKEN_STATUS,
+          payload: 'success'
+        })
+      );
+      break;
+    case 'done':
+      dispatch({
+        type: TRANSFER_NAME_STATUS,
+        payload: 'success'
+      });
+      dispatch({ type: MINT_TOKEN_STATUS, payload: 'success' });
+      break;
+    default:
+      break;
+  }
+};
+
+export const untokenizeSubmitTransaction = name => async dispatch => {
+  const label = formatENSDomain(name).match(/(.*)\.eth/)[1];
   const step = await getNextRegisterStep(Web3.utils.keccak256(label));
   switch (step) {
     case 'transfer':
@@ -66,7 +110,8 @@ export const tokenizeSubmitTransaction = name => async dispatch => {
 const INITIAL_STATE = {
   input: '',
   transferNameStatus: '',
-  mintTokenStatus: ''
+  mintTokenStatus: '',
+  burnTokenStatus: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -82,6 +127,11 @@ export default (state = INITIAL_STATE, action) => {
         transferNameStatus: action.payload
       };
     case MINT_TOKEN_STATUS:
+      return {
+        ...state,
+        mintTokenStatus: action.payload
+      };
+    case BURN_TOKEN_STATUS:
       return {
         ...state,
         mintTokenStatus: action.payload
