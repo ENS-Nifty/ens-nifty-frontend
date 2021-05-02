d13d1e0e8c2da0fd705648abdd5df92579204bec
billy rennekamp <billy.rennekamp@gmail.com>
2021-05-02 13:14:18 +0200

    does this do anything?

--- functions/metadata.js
+++ functions/metadata.js
@@ -58,6 +58,30 @@ exports.handler = (event, context, cb) => {
       .padStart(64, '0');
 
   const labelHash = event.queryStringParameters.hash.toLowerCase();
+
+  // timdaub.eth
+  if (labelHash == '0x3bf87c5c609b6a0e5b0daa400c18c396b1db1c927e55a0e1d61405b756e2b0b8') {
+    let imageUrl = 'https://picsum.photos/200'
+    let homeUrl = 'https://google.com'
+    return res
+        .status(200)
+        .set(headers)
+        .send({
+          name: 'foo bar',
+          description: `foo baz`,
+          image: imageUrl,
+          external_url: homeUrl,
+          // background_color: 'FFFFFF',
+          // attributes: { lockedEther: parseFloat(lockedEther), dateRegistered }
+          image_url: imageUrl,
+          home_url: homeUrl,
+          // properties: [
+          //   { key: 'locked-ether', value: parseFloat(lockedEther), type: 'integer' },
+          //   { key: 'date-registered', value: dateRegistered, type: 'string' }
+          // ]
+        })
+  }
+  
   return client
     .query(q.Get(q.Match(q.Index('domain_by_label_hash'), labelHash)))
     .then(ret => ret.data)
